8d9c5805cb33080e6c69a6b99662ae7dc44b5635
Emiliano Rosso <arraydude@gmail.com>
2022-09-21 16:17:10 -0300

    Ilustración

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ for i in days_list:
             st.markdown(f.read())
         if os.path.isfile(f'content/figures/{j}.csv') == True:
             st.markdown('---')
-            st.markdown('### Figures')
+            st.markdown('### Ilustraciones')
             df = pd.read_csv(f'content/figures/{j}.csv', engine='python')
             for i in range(len(df)):
                 st.image(f'content/images/{df.img[i]}')
